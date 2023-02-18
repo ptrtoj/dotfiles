1929ee153276339a232f8a4e937db9e4afdb3388
WooHyung Jeon <jeon@ptrtoj.com>
2023-02-18 17:13:32 +0900

    Cleaned up

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -1,36 +1,58 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-	use ({
-		'shaunsingh/nord.nvim',
-		commit = "78f5f001709b5b321a35dcdc44549ef93185e024"})
+  use 'tpope/vim-fugitive'
 
-	-- dependency
-	use 'kyazdani42/nvim-web-devicons'
-		use 'nvim-lualine/lualine.nvim'
-		use 'kyazdani42/nvim-tree.lua'
-	-- end
+  use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
-	use 'windwp/nvim-autopairs'
-	use 'mhinz/vim-startify'
-	use 'DanilaMihailov/beacon.nvim'
-	use 'norcalli/nvim-colorizer.lua'
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},             -- Required
+          {'williamboman/mason.nvim'},           -- Optional
+          {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},         -- Required
+          {'hrsh7th/cmp-nvim-lsp'},     -- Required
+          {'hrsh7th/cmp-buffer'},       -- Optional
+          {'hrsh7th/cmp-path'},         -- Optional
+          {'saadparwaiz1/cmp_luasnip'}, -- Optional
+          {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},             -- Required
+          {'rafamadriz/friendly-snippets'}, -- Optional
+      }
+  }
+
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  use 'junegunn/seoul256.vim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
-
