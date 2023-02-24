a0425bacb9efa4c12e64084c89ebf0d14bf32b15
WooHyung Jeon <jeon@ptrtoj.com>
2023-02-24 23:50:20 +0900

    Fixed neovim error

--- configs/nvim/lua/pkg.lua
+++ configs/nvim/lua/pkg.lua
@@ -15,36 +15,35 @@ return require('packer').startup(function(use)
   -- Package Manager itself
   use 'wbthomason/packer.nvim'
 
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
-  use
   -- Requires system package 'ripgrep'
   -- And then, do ':checkhealth telescope'
-  use {'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'}}}
+  use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}}}
 
   use {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v1.x',
       requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},             -- Required
-          -- Requires system package
-          -- 'git', 'curl || wget', 'unzip', 'tar', 'gzip'
-          -- And then, do ':checkhealth mason'
-          {'williamboman/mason.nvim'},           -- Optional
-          {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	  -- LSP Support
+	  {'neovim/nvim-lspconfig'},             -- Required
+	  -- Requires system package
+	  -- 'git', 'curl || wget', 'unzip', 'tar', 'gzip'
+	  -- And then, do ':checkhealth mason'
+	  {'williamboman/mason.nvim'},           -- Optional
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},         -- Required
-          {'hrsh7th/cmp-nvim-lsp'},     -- Required
-          {'hrsh7th/cmp-buffer'},       -- Optional
-          {'hrsh7th/cmp-path'},         -- Optional
-          {'saadparwaiz1/cmp_luasnip'}, -- Optional
-          {'hrsh7th/cmp-nvim-lua'},     -- Optional
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},         -- Required
+	  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+	  {'hrsh7th/cmp-buffer'},       -- Optional
+	  {'hrsh7th/cmp-path'},         -- Optional
+	  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+	  {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},             -- Required
-          {'rafamadriz/friendly-snippets'}, -- Optional
+	  -- Snippets
+	  {'L3MON4D3/LuaSnip'},             -- Required
+	  {'rafamadriz/friendly-snippets'}, -- Optional
       }
   }
 
