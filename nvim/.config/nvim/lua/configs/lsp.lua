73d91407d02a612c030186b97fce8d9bee1bd722
WooHyung Jeon <jeon@ptrtoj.com>
2023-02-27 02:12:17 +0900

    Changed: python lsp server

--- nvim/.config/nvim/lua/configs/lsp.lua
+++ nvim/.config/nvim/lua/configs/lsp.lua
@@ -36,7 +36,7 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require('lspconfig')['pyright'].setup{
+require('lspconfig')['pylsp'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
